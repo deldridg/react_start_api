d13aa0ae0f31542a12e5a885cb3c98aae2d927b2
David Eldridge <deldridg@ozemail.com.au>
2026-06-24 22:18:42 +1000

    Fix build.sh — clone React repo from GitHub on Render

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-# Install Python dependencies
+# Install Python deps
 pip install -r requirements.txt
 
-# Build React - assumes react_start is a sibling directory to react_start_api
-cd ../react_start
+# Clone React repo and build it
+git clone https://github.com/deldridg/react_start.git /tmp/react_start
+cd /tmp/react_start
 npm install
 npm run build
 
 # Copy React build into Django
-cp -r build ../react_start_api/react_build
+cp -r dist/ $OLDPWD/react_build
 
 # Back to Django
-cd ../react_start_api
+cd $OLDPWD
 python manage.py collectstatic --noinput
 python manage.py migrate
