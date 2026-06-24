0d0a635010c49bf86304fe289b2176d4f4e9f66e
David Eldridge <deldridg@ozemail.com.au>
2026-06-24 22:01:47 +1000

    Add Render deployment config and whitenoise

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# React build - served from Django
+REACT_BUILD_DIR = os.path.join(BASE_DIR, 'react_build')
+
+TEMPLATES[0]['DIRS'] = [REACT_BUILD_DIR]
--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from sales.views import sales_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sales/', sales_data, name='sales_data'),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
